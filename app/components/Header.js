81af1ca65bcd87bc410ba04b79eb00475e24ca27
saurav <sauravmalekoo07@gmail.com>
2026-06-06 19:11:10 +0545

    to vercel

---
+++ app/components/Header.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '@/public/Inkora.png'
+
+export default function Header() {
+    return (
+        <>
+
+            <header className="w-325 mx-auto">
+                <nav className="flex items-center">
+                    <Link href="/">
+                        <Image src={logo} alt="Inkora" width={150} height={150}/>
+                    </Link>
+                    <ul className="flex items-center list-unstyled gap-2 font-urbanist">
+                        <Link href="/">Home</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/blog">Blog</Link>
+                        <Link href="/services">Services</Link>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,65 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
+import banner from '@/public/banner.jpeg'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+
+    return (
+        <>
+            <div className="absolute top-0 w-full overflow-hidden z-[-2] bg-cover bg-center bg-no-repeat object-cover">
+                <Image src={banner} alt="banner" className="w-full h-full object-cover "/>
+            </div>
+            <main
+                className="font-urbanist w-325 mx-auto flex overflow-hidden items-center py-[8rem]">
+                <div className="w-162.5">
+                    <h1 className="font-black text-[60px]  leading-tight">Best <span className="text-[#21a4d9]"> Printing Service </span>
+                        <span className="text-[#132353]"> Company in Nepal</span>
+                    </h1>
+                    <p className="my-6 text-[18px]">We make things that look really good like pictures and words and we
+                        print them
+                        on paper or other
+                        things. Our graphic design and print solutions are for businesses organizations and
+                        individuals. </p>
+                    <Link href="/contact"
+                          className="bg-[#132353] text-white p-3 rounded-full hover:bg-[#21a4d9] ease-in-out duration-500">Get
+                        Started</Link>
+                </div>
+                <div>
+
+                </div>
+            </main>
+
+
+        </>
+    );
 }
